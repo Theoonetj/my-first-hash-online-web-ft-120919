a95b08f1637f04a56d131fcbd0241893e0e9d5e6
confident-token-1019 <theoonetj@gmail.com>
2019-12-19 17:32:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,10 +21,7 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+                  hash[oil paintings]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
