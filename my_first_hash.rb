11ad6d70f818580f08cf14da9ee5af694382a632
confident-token-1019 <theoonetj@gmail.com>
2019-12-19 17:03:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,3 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
 def my_hash 
@@ -11,10 +9,6 @@ def my_hash
 end
 
 
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
 def shipping_manifest
   the_manifest = {
@@ -32,7 +26,7 @@ end
                  shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+end
 def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
